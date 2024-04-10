3753760cfe807b2ddd8ae7d7e31661eb3e9557da
doug <doug@texanforged.com>
2024-04-10 14:29:55 -0500

    Adding a drush file.

--- web/modules/custom/ark_server_status/src/Drush/Commands/ArkServerStatusCommands.php
+++ web/modules/custom/ark_server_status/src/Drush/Commands/ArkServerStatusCommands.php
@@ -17,57 +17,26 @@ final class ArkServerStatusCommands extends DrushCommands {
   /**
    * Constructs an ArkServerStatusCommands object.
    */
-  public function __construct(
-    private readonly Token $token,
-    private readonly LoggerChannelFactoryInterface $loggerFactory,
-  ) {
+  public function __construct(Token $token, LoggerChannelFactoryInterface $loggerFactory) {
     parent::__construct();
   }
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container) {
-    return new static(
+  public static function create(ContainerInterface $container): ArkServerStatusCommands {
+    return new ArkServerStatusCommands(
       $container->get('token'),
       $container->get('logger.factory'),
     );
   }
 
   /**
-   * Command description here.
+   * Provides server status.
    */
-  #[CLI\Command(name: 'ark_server_status:command-name', aliases: ['foo'])]
-  #[CLI\Argument(name: 'arg1', description: 'Argument description.')]
-  #[CLI\Option(name: 'option-name', description: 'Option description')]
-  #[CLI\Usage(name: 'ark_server_status:command-name foo', description: 'Usage description')]
-  public function commandName($arg1, $options = ['option-name' => 'default']) {
-    $this->logger()->success(dt('Achievement unlocked.'));
+  public function getArkServerStatus() {
+    //Checks to see if the server is on.
+    //Send alert email.
+    //Document that the server is on.
   }
-
-  /**
-   * An example of the table output format.
-   */
-  #[CLI\Command(name: 'ark_server_status:token', aliases: ['token'])]
-  #[CLI\FieldLabels(labels: [
-    'group' => 'Group',
-    'token' => 'Token',
-    'name' => 'Name'
-  ])]
-  #[CLI\DefaultTableFields(fields: ['group', 'token', 'name'])]
-  #[CLI\FilterDefaultField(field: 'name')]
-  public function token($options = ['format' => 'table']): RowsOfFields {
-    $all = $this->token->getInfo();
-    foreach ($all['tokens'] as $group => $tokens) {
-      foreach ($tokens as $key => $token) {
-        $rows[] = [
-          'group' => $group,
-          'token' => $key,
-          'name' => $token['name'],
-        ];
-      }
-    }
-    return new RowsOfFields($rows);
-  }
-
 }
